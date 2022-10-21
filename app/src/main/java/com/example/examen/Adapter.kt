7cb1b5d76b8500b879450e6ea1178f3ba46190e8
MarcoRuizz <mruiz22@ucol.mx>
2022-10-20 19:20:34 -0500

    - delete database register - listview adapter - save database to arrays

--- app/src/main/java/com/example/examen/Adapter.kt
+++ app/src/main/java/com/example/examen/Adapter.kt
@@ -1,11 +1,28 @@
 package com.example.examen
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.app.Activity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
 
-class Adapter : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_adapter)
+class Adapter(private val context: Activity, private val imageID: Array<Int>, private val nombre: Array<String>, private val descripcion: Array<String>)
+    : ArrayAdapter<String>(context, R.layout.custom_list, nombre) {
+
+    override fun getView(position: Int, view: View?, parent: ViewGroup): View {
+        val inflater = context.layoutInflater
+
+        val rowView = inflater.inflate(R.layout.custom_list, null, true)
+
+        val titleText = rowView.findViewById(R.id.title) as TextView
+        val imageView = rowView.findViewById(R.id.icon) as ImageView
+        val subtitleText = rowView.findViewById(R.id.description) as TextView
+
+        titleText.text = nombre[position]
+        imageView.setImageResource(imageID[position])
+        subtitleText.text = descripcion[position]
+
+        return rowView
     }
 }
